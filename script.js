d8dd929bf93bce8e3b532d32050a4dd63e2a768e
Brittany Walker <breneewalker@gmail.com>
2019-05-12 23:57:17 -0400

    add smooth scroll, clean up css

--- script.js
+++ script.js
@@ -14,3 +14,16 @@ function randomMusicalLyrics(lyrics){
 
 randomMusicalLyrics(lyrics)
 
+$(document).ready(function(){
+	$("a").on('click', function(event) {
+	  if (this.hash !== "") {
+		event.preventDefault();
+		var hash = this.hash;
+		$('html, body').animate({
+		  scrollTop: $(hash).offset().top
+		}, 1000, function(){
+		  window.location.hash = hash;
+		});
+	  } 
+	});
+  });
